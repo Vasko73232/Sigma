bcf35be4724f6236c0fe0f5f40e65057cda6586c
Dima Goltyapov <43724581+Vasko73232@users.noreply.github.com>
2021-12-22 23:14:37 +0500

    Add files via upload

---
+++ Sigma/Sigma/Views/RegistrationPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Sigma.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegistrationPage : ContentPage
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+        }
+        private async void Login(object sender, System.EventArgs e)
+        {
+            string loginStatus = "isSuccess";
+
+
+
+            if (loginStatus == "isSuccess") {
+                Application.Current.MainPage = new AppShell();
+            }
+            else
+            {
+                DisplayAlert("Sorry", "Something went wrong in server.", "Ok");
+            }
+        }
+        private async void Registration(object sender, System.EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
